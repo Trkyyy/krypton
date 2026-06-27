1e8c6f0a4313b654a06893978bc9e4f434d481b1
Trkyyy <57319969+Trkyyy@users.noreply.github.com>
2026-06-27 09:32:45 +0100

    Correct typo

--- src/main/java/me/steinborn/krypton/mixin/shared/network/pipeline/encryption/ConnectionMixin.java
+++ src/main/java/me/steinborn/krypton/mixin/shared/network/pipeline/encryption/ConnectionMixin.java
@@ -23,7 +23,7 @@ public class ConnectionMixin implements ClientConnectionEncryptionExtension {
 
     @Override
     public void setupEncryption(SecretKey key) throws GeneralSecurityException {
-        if (this.kyptonEncryptionEnabled) {
+        if (this.kryptonEncryptionEnabled) {
             return;
         }
 
